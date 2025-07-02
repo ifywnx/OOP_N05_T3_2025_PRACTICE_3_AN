ece6de392e5ffeadc490926fbca4924b86a55c78
ifywnx <annguyen.ub0@gmail.com>
2025-07-02 07:39:51 +0000

    update

--- test/TestUser.java
+++ test/TestUser.java
@@ -3,6 +3,14 @@ public class TestUser{
         User user = new User();
         user.setTen("NguyenVanAn");
         user.getTen();
-        
+        user.setTuoi(19);
+        user.setEmail("an@gmail.com");
+        user.setSDT("57897547");
+        user.setQueQuan("HungYen");
+        user.getTuoi();
+        user.getEmail();
+        user.getSDT();
+        user.getQueQuan();
+
     }
 }
